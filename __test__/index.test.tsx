39031a823579859f4cedbcd4668219e622d050b0
JJWJ <ambitionwins@gmail.com>
2022-03-09 20:38:12 -0800

    Update tests for Netflix

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import Header from '../components/display/Header'
 import Home from '../pages/index'
 
 describe('Home', () => {
@@ -6,9 +7,20 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /Hi/i
+      name: /Unlimited movies, TV shows, and more./i
     })
 
     expect(heading).toBeInTheDocument()
   })
+
+  it('renders the header', () => {
+    render(<Header />)
+
+    const check_header = screen.getByRole('option', {
+      name: /English/i
+    })
+    expect(check_header).toBeInTheDocument()
+  })
+
+
 })
