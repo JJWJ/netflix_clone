f6796ea3f0631733e35510f336b29a7c337aa0aa
JJWJ <ambitionwins@gmail.com>
2022-03-09 20:38:59 -0800

    Add playwright test to check page title

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -4,5 +4,13 @@ test('basic test', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
   // The start page should have our h1 with hi
-  await expect(page.locator('h1')).toContainText('Hi')
+  await expect(page.locator('h1')).toContainText('Unlimited movies, TV shows, and more.')
+})
+
+test('Page title correct',async ({ page }) => {
+  await page.goto('/')
+
+  // Expect the title "to contain" the substring
+  await expect(page).toHaveTitle(/Netflix Clone/)
+
 })
