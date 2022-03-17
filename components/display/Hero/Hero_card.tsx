f5b7debb97b8070c45289e443dd71ce963710086
JJWJ <ambitionwins@gmail.com>
2022-03-17 11:03:53 -0700

    Fix style for hero card background containment and flow

--- components/display/Hero/Hero_card.tsx
+++ components/display/Hero/Hero_card.tsx
@@ -8,17 +8,13 @@ import Hero_card_text from './Hero_card_text'
 
 const Hero_card = () => {
     return (
-        <React.Fragment>
-            <div className='flex w-screen justify-center items-center content-center '>
-                <Header />
-                <Hero_card_background />
-                <div className='flex py-20  flex-col w-full justify-center items-center'>
-                    <Hero_card_text />
-                    <Hero_card_form />
-                </div>
-                <div className='absolute top-0 left-0 bottom-0 right-0 w-full h-[70px] z-0 bg-[rgba(0,0,0,.4)] bg-gradient-to-t from-[rgba(0,0,0,.2)]  via-[rgba(0,0,0,.8)] to-[rgb(0,0,0)]'></div>
+        <div className='flex w-screen justify-center items-center content-center '>
+            <Hero_card_background />
+            <div className='flex py-20  flex-col w-full justify-center items-center '>
+                <Hero_card_text />
+                <Hero_card_form />
             </div>
-        </React.Fragment>
+        </div>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Footer_card from '../components/display/Footer/Footer_card'
+import Header from '../components/display/Header/Header'
 import Hero_card from '../components/display/Hero/Hero_card'
 
 const Home: NextPage = () => {
   return (
-    <div className='w-screen h-screen bg-black overflow-hidden'>
+    <div className='w-screen h-screen bg-black'>
       <Head>
         <title>Netflix Clone</title>
       </Head>
-      <main className=' overflow-x-hidden'>
-        <Hero_card />
-        {/* <Hero_card_text /> */}
+      <main className='relative overflow-x-hidden '>
+        <div className='relative top-0 left-0f bottom-0 right-0 z-10 '>
+          <div className='max-w-[1920p] justify-center flex'>
+            <Header />
+          </div>
+          <div className='absolute top-0 left-0 bottom-0 right-0 w-full h-[70px] z-0 bg-[rgba(0,0,0,.4)] bg-gradient-to-t from-[rgba(0,0,0,.2)]  via-[rgba(0,0,0,.8)] to-[rgb(0,0,0)]'></div>
+        </div>
+        <div className='flex h-screen'>
+          <Hero_card />
+        </div>
+        <Footer_card />
       </main>
     </div >
   )
