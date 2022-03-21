4b28caee8a2cd42dacd9069e5c50ae685bcb3211
JJWJ <ambitionwins@gmail.com>
2022-03-21 10:53:52 -0700

    Update hero card styles second pass

--- components/display/Hero/Hero_card_form.tsx
+++ components/display/Hero/Hero_card_form.tsx
@@ -4,17 +4,17 @@ const Hero_card_form = () => {
     return (
         <div className='z-10 text-white text-center flex justify-center w-full'>
             <form action="" className='w-full flex flex-col justify-center items-center'>
-                <h3 className='text-lg  text-center px-[5%] pb-2 max-w-[450px]'>Ready to watch? Enter your email to create or restart your membership.</h3>
+                <h3 className='text-lg  text-center px-[5%] pb-2 max-w-[450px] lg:max-w-[900px]'>Ready to watch? Enter your email to create or restart your membership.</h3>
                 <div className='flex flex-row flex-wrap justify-center'>
                     {/* Relative Position on the div with absolute for the contents */}
                     <div className=' relative'>
                         <label className='absolute top-[30%] left-[10px] text-gray-700' htmlFor="email_hero">
                             Email address
                         </label>
-                        <input className='min-w-[350px] max-w-[950px] py-3  rounded-sm border-none outline-none my-2' type="email" name="" id="email_hero" maxLength={50} minLength={5} tabIndex={0} autoComplete="email" />
+                        <input className='min-w-[350px] max-w-[950px] py-3 xl:min-w-[500px] xl:min-h-[60px]  rounded-sm border-none outline-none my-2 text-black' type="email" name="" id="email_hero" maxLength={50} minLength={5} tabIndex={0} autoComplete="email" />
                     </div>
                     <div className='my-2'>
-                        <button className='flex px-6 py-3 bg-[#e50914] min-w-[74px] rounded-sm min-h-[40px] mx-auto'>Get Started
+                        <button className='flex px-6 py-3 bg-[#e50914] min-w-[74px] rounded-sm min-h-[40px] mx-auto xl:min-h-[60px]'>Get Started
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
                             </svg>
--- components/display/Hero/Hero_card_text.tsx
+++ components/display/Hero/Hero_card_text.tsx
@@ -1,9 +1,9 @@
 
 const Hero_card_text = () => {
     return (
-        <div className="text-white z-10 pt-10 pb-2 w-full max-w-[950px] text-center ">
-            <h1 className="text-3xl font-bold ">Unlimited movies, TV shows, and more.</h1>
-            <h2 className="text-lg my-2">Watch anywhere. Cancel anytime.</h2>
+        <div className="text-white z-10 pt-10 pb-2 w-full max-w-lg text-center flex justify-center items-center flex-col">
+            <h1 className="text-3xl font-bold xl:text-5xl max-w-[800px] text-center">Unlimited movies, TV shows, and more.</h1>
+            <h2 className="text-lg my-2 xl:text-2xl">Watch anywhere. Cancel anytime.</h2>
         </div>
     )
 }
