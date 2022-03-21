39db7d5820b43b0f10be8e0a5ea6f1ba29dbb572
JJWJ <ambitionwins@gmail.com>
2022-03-21 10:52:26 -0700

    Make footer styles responsive

--- components/display/Footer/Footer_card.tsx
+++ components/display/Footer/Footer_card.tsx
@@ -5,55 +5,55 @@ const Footer_card = () => {
             <div className="flex flex-col justify-start items-start m-8">
                 <p className="mb-4">Questions? Call  <a href="tel:1-844-505-2993">1-844-505-2993</a></p>
                 <ul className=" flex flex-row max-w-screen flex-wrap">
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">FAQ</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Help Center</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Account</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Media Center</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Investor Relations</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Jobs</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Redeem Gift Cards</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Buy Gift Cards</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Ways to Watch</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Terms of Use</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Privacy</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Cookie Preferences</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Corporate Information</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Contact Us</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Speed Test</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Legal Notices</a>
                     </li>
-                    <li className="w-1/2 pr-3 mb-4">
+                    <li className="w-1/2 sm:w-1/3 md:w-1/4 pr-3 mb-4">
                         <a href="#">Only on Netflix</a>
                     </li>
                 </ul>
